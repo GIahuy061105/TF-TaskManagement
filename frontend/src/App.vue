8ba918c5837dd60e6688085a84fa8473cf3ab682
Sudaukecoder <nguyengiahu29@gmail.com>
2026-04-03 23:22:05 +0700

    1: Base Frontend , Backend , Database

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-slate-900 flex items-center justify-center">
+    <h1 class="text-5xl font-bold !text-sky-400 drop-shadow-lg">
+      TaskFlow is Ready! 🚀
+    </h1>
+  </div>
+</template>
