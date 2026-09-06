9962dd1090d5dd229ab19305aaf0b15dcb4419cb
Sudaukecoder <nguyengiahu29@gmail.com>
2026-09-06 21:15:38 +0700

    15.5: Bring Project to Internet

--- backend/src/services/mailer.service.js
+++ backend/src/services/mailer.service.js
@@ -1,9 +1,15 @@
 import nodemailer from 'nodemailer'
 const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST || 'smtp.gmail.com',
+  port: process.env.SMTP_PORT || 465,
+  secure: true,
   service: 'gmail',
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
+  },
+  tls: {
+      rejectUnauthorized: false
   }
 })
 
