49f4ee58a4ce1ec802958220398afd6f19ddcbc5
Sudaukecoder <nguyengiahu29@gmail.com>
2026-09-07 13:19:15 +0700

    17: Update RealTime with Socket.io

--- backend/src/plugins/index.js
+++ backend/src/plugins/index.js
@@ -1,7 +1,7 @@
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import cookie from '@fastify/cookie'
-
+import { Server } from 'socket.io'
 export async function registerPlugins(app) {
   await app.register(cors, {
   origin: 'https://tf-taskmanagement.vercel.app',
@@ -22,5 +22,19 @@ export async function registerPlugins(app) {
   app.addHook('onSend', async (request, reply) => {
       reply.header('Cross-Origin-Opener-Policy', 'same-origin-allow-popups')
       reply.header('Cross-Origin-Embedder-Policy', 'unsafe-none')
+    })
+  const io = new Server(app.server, {
+      cors: {
+        origin: ['http://localhost:5173', 'https://tf-taskmanagement.vercel.app'],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
+      }
+  })
+  app.decorate('io', io)
+
+  io.on('connection', (socket) => {
+        console.log('🟢 Một client đã kết nối Socket:', socket.id)
+      socket.on('disconnect', () => {
+        console.log('🔴 Client đã ngắt kết nối:', socket.id)
+      })
     })
 }
